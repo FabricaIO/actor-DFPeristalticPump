a5a28975ae2a1c37d8e8168e7b5040221c317b67
Garth Johnson <growlf@thenetyeti.com>
2024-09-26 18:56:29 -0700

    initial import

---
+++ src/DFPeristalticPump.cpp
@@ -0,0 +1,137 @@
+#include "DFPeristalticPump.h"
+
+/// @brief Creates a peristaltic pump object
+/// @param ConfigFile The file name to store settings in
+DFPeristalticPump::DFPeristalticPump(String ConfigFile) {
+	path = "/settings/sig/" + ConfigFile;
+	// Allow allocation of all timers
+    ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+}
+
+/// @brief Starts a peristaltic pump object
+/// @return True on success
+bool DFPeristalticPump::begin() {
+	// Set description
+	Description.signalQuantity = 1;
+	Description.type = "pump";
+	Description.name = "Peristaltic Pump";
+	Description.signals = {{"Dose", 0}};
+	Description.id = 3;
+	pump.setPeriodHertz(50);
+	bool result = false;
+	// Create settings directory if necessary
+	if (!checkConfig(path)) {
+		// Set defaults
+		result = setConfig(R"({"pumpSpeed": 180, "doseTime": 2000, "pin": 26, "threshold": 50, "autoParameter": "", "autoEnabled": false, "activeLow": true, "taskName": "AutoPump", "taskPeriod": 10000 })");
+	} else {
+		// Load settings
+		result = setConfig(Storage::readFile(path));
+	}
+	return result;
+}
+
+/// @brief Receives a signal
+/// @param signal The signal to process (only option is 0 for get data)
+/// @param payload Not used
+/// @return A plaintext response with the data
+std::tuple<bool, String> DFPeristalticPump::receiveSignal(int signal, String payload) {
+	dose();
+	return { true, R"({"success": true})" };
+}
+
+/// @brief Activates the pump for the configured amount of time
+void DFPeristalticPump::dose() {
+	pump.write(current_config.pumpSpeed);
+	delay(current_config.doseTime);
+	pump.write(90);
+}
+
+/// @brief Runs the auto pump task
+/// @param elapsed The amount of time, in ms, since this was last called
+void DFPeristalticPump::runTask(long elapsed) {
+	totalElapsed += elapsed;
+	if (totalElapsed > TaskDescription.taskPeriod) {
+		totalElapsed = 0;
+		for (const auto& m : SensorManager::measurements) {
+			if (m.parameter == current_config.autoParameter) {
+				if (current_config.activeLow) {
+					if (m.value < current_config.threshold) {
+						dose();
+						return;
+					}
+				} else {
+					if (m.value > current_config.threshold) {
+						dose();
+						return;
+					}
+				}
+			}
+		}
+	}
+}
+
+/// @brief Enables the auto pump
+/// @param enable True to enable, false to disable 
+/// @return True on success
+bool DFPeristalticPump::enableAuto(bool enable) {
+	current_config.autoEnabled = enable;
+	return enableTask(enable);
+}
+
+/// @brief Gets the current config
+/// @return A JSON string of the config
+String DFPeristalticPump::getConfig() {
+	// Allocate the JSON document
+	JsonDocument doc;
+	// Assign current values
+	doc["pumpSpeed"] = current_config.pumpSpeed;
+	doc["autoParameter"] = current_config.autoParameter;
+	doc["doseTime"] = current_config.doseTime;
+	doc["threshold"] = current_config.threshold;
+	doc["pin"] = current_config.pin;
+	doc["autoEnabled"] = current_config.autoEnabled;
+	doc["activeLow"] = current_config.activeLow;
+	doc["taskName"] = TaskDescription.taskName;
+	doc["taskPeriod"] = TaskDescription.taskPeriod;
+
+	// Create string to hold output
+	String output;
+	// Serialize to string
+	serializeJson(doc, output);
+	return output;
+}
+
+/// @brief Sets the configuration for this device
+/// @param config The JSON config to use
+/// @return True on success
+bool DFPeristalticPump::setConfig(String config) {
+	// Allocate the JSON document
+  	JsonDocument doc;
+	// Deserialize file contents
+	DeserializationError error = deserializeJson(doc, config);
+	// Test if parsing succeeds.
+	if (error) {
+		Serial.print(F("Deserialization failed: "));
+		Serial.println(error.f_str());
+		return false;
+	}
+	// Assign loaded values
+	current_config.pumpSpeed = doc["pumpSpeed"].as<int>();
+	current_config.autoParameter = doc["autoParameter"].as<String>();
+	current_config.doseTime = doc["doseTime"].as<int>();
+	current_config.threshold = doc["threshold"].as<int>();
+	current_config.pin = doc["pin"].as<int>();
+	current_config.autoEnabled = doc["autoEnabled"].as<bool>();
+	current_config.activeLow = doc["activeLow"].as<bool>();
+	TaskDescription.taskName = doc["taskName"].as<std::string>();
+	TaskDescription.taskPeriod = doc["taskPeriod"].as<long>();
+	pump.detach();
+	pump.attach(current_config.pin, 500, 2500);
+	if (!saveConfig(path, config)) {
+		return false;
+	}
+	return enableAuto(current_config.autoEnabled);
+}
---
+++ src/DFPeristalticPump.h
@@ -0,0 +1,65 @@
+/*
+* This file and associated .cpp file are licensed under the GPLv3 License Copyright (c) 2024 Sam Groveman and Garth Johnson
+* 
+* External libraries needed:
+* ArduinoJSON: https://arduinojson.org/ 
+* ESP32Servo: https://github.com/madhephaestus/ESP32Servo
+*
+* DFRobot Peristaltic Pump: https://www.dfrobot.com/product-1698.html
+*
+* Contributors: Sam Groveman, Garth Johnson
+*/
+
+#pragma once
+#include <Arduino.h>
+#include <SignalReceiver.h>
+#include <SensorManager.h>
+#include <Storage.h>
+#include <ArduinoJson.h>
+#include <ESP32Servo.h>
+#include <PeriodicTask.h>
+
+/// @brief Allows for control of a peristaltic pump
+class DFPeristalticPump : public SignalReceiver, public PeriodicTask {
+	private:
+		/// @brief Holds pump configuration
+		struct {
+			/// @brief Speed at which pump runs
+			int pumpSpeed;
+
+			/// @brief Time in ms for each dose
+			int doseTime;
+
+			/// @brief Pin for pump
+			int pin;
+
+		    /// @brief threshold below which to deliver a dose
+		    int threshold;
+			
+			/// @brief Name of the sensor to use for auto-triggering
+			String autoParameter;
+
+			/// @brief Whether to enable auto triggering or not
+			bool autoEnabled;
+
+			/// @brief Whether the pump should be active on the above the threshold (false) or below (true)
+			bool activeLow; 
+			
+		} current_config;
+
+		/// @brief Full path to config file
+		String path;
+		
+		/// @brief Servo object
+		Servo pump;
+
+		void dose();
+		bool enableAuto(bool enabled);
+	public:
+		DFPeristalticPump(String ConfigFile = "DFPump.json");
+		bool begin();
+		std::tuple<bool, String> receiveSignal(int signal, String payload = "");
+		void runTask(long elapsed);
+		String getConfig();
+		bool setConfig(String config);
+};
